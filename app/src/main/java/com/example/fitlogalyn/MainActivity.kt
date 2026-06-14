0530e8eb3baadec6d0140ba85bb18fc468313ba0
Alyn Rivera Abreu <alyn.rivera.abreu.1@comunidad.unam.mx>
2026-06-13 22:01:34 -0600

    Agregar MainActivity y AgregarEjercicioActivity con navegación

--- app/src/main/java/com/example/fitlogalyn/MainActivity.kt
+++ app/src/main/java/com/example/fitlogalyn/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.fitlogalyn
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var db: FitLogDatabase
+    private lateinit var adapter: EjercicioAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        db = FitLogDatabase.getDatabase(this)
+
+        adapter = EjercicioAdapter(emptyList()) { ejercicio ->
+            lifecycleScope.launch {
+                db.ejercicioDao().eliminar(ejercicio)
+            }
+        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        db.ejercicioDao().obtenerTodos().observe(this, Observer { lista ->
+            adapter.actualizarLista(lista)
+        })
+
+        findViewById<FloatingActionButton>(R.id.fabAgregar).setOnClickListener {
+            startActivity(Intent(this, AgregarEjercicioActivity::class.java))
         }
     }
 }
